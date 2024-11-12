4944e676b612c8702cebe501878791b9b17bbec8
Kalle <kalle@kandru.de>
2024-11-12 18:16:04 +0100

    bump version to 0.0.17

--- src/Version.cs
+++ src/Version.cs
@@ -4,6 +4,6 @@ namespace MapModifiers
 {
     public partial class MapModifiers : BasePlugin
     {
-        public override string ModuleVersion => "0.0.16";
+        public override string ModuleVersion => "0.0.17";
     }
 }
