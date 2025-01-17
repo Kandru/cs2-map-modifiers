1d2f0451dcff077ec7d2cb85b6fbb19852271781
Kalle <info@kalleminkner.de>
2025-01-17 12:40:02 +0100

    bump version to 0.0.28

--- src/Version.cs
+++ src/Version.cs
@@ -4,6 +4,6 @@ namespace MapModifiers
 {
     public partial class MapModifiers : BasePlugin
     {
-        public override string ModuleVersion => "0.0.27";
+        public override string ModuleVersion => "0.0.28";
     }
 }
